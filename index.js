998b9a510cd4751e9c62db957856cd151398d6e5
Damien Irouva <dex.ir93@gmail.com>
2025-01-24 13:37:33 +0100

    Prepare runner group

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 const core = require('@actions/core');
 const httpClient = require('@actions/http-client');
 
-async function checkRunner({ token, owner, repo, primaryRunnerLabels, fallbackRunner }) {
+
+async function listRunnersInGroup({ org, runnerGroupId, token }) {
+  // Docs: https://docs.github.com/en/rest/actions/self-hosted-runners#list-all-self-hosted-runners-in-a-group-for-an-organization
+  const http = new httpClient.HttpClient('http-client');
+  const headers = { 'Authorization': `Bearer ${token}` };
+
+  // GET /orgs/{org}/actions/runner-groups/{runner_group_id}/runners
+  const url = `https://api.github.com/orgs/${org}/actions/runner-groups/${runnerGroupId}/runners`;
+  const response = await http.getJson(url, headers);
+
+  if (response.statusCode !== 200) {
+    throw new Error(`Failed to list runners in group. Status code: ${response.statusCode}`);
+  }
+
+  const { runners = [] } = response.result;
+  return runners;
+}
+
+async function getRunnerGroupId({ org, runnerGroupName, token }) {
+  // This will list the runner groups in the org.
+  // Docs: https://docs.github.com/en/rest/actions/self-hosted-runners#list-self-hosted-runner-groups-for-an-organization
+  const http = new httpClient.HttpClient('http-client');
+  const headers = { 'Authorization': `Bearer ${token}` };
+
+  // GET /orgs/{org}/actions/runner-groups
+  const url = `https://api.github.com/orgs/${org}/actions/runner-groups`;
+  const response = await http.getJson(url, headers);
+
+  if (response.statusCode !== 200) {
+    throw new Error(`Failed to list runner groups. Status code: ${response.statusCode}`);
+  }
+
+  const { runner_groups: runnerGroups = [] } = response.result;
+  const group = runnerGroups.find(g => g.name === runnerGroupName);
+
+  if (!group) {
+    throw new Error(`Runner group "${runnerGroupName}" not found in org "${org}".`);
+  }
+
+  return group.id;
+}
+
+async function checkRunner({ token, owner, repo, primaryRunnerLabels, fallbackRunner, runnerGroup }) {
   const http = new httpClient.HttpClient('http-client');
   const headers = {
     'Authorization': `Bearer ${token}`,
@@ -27,6 +69,11 @@ async function checkRunner({ token, owner, repo, primaryRunnerLabels, fallbackRu
     }
   }
 
+  if (!primaryIsOnline && runnerGroup) {
+    const groupId = await getRunnerGroupId({ org: owner, runnerGroupName: runnerGroup, token });
+    useRunner = await listRunnersInGroup({ org: owner, runnerGroupId: groupId, token });
+  }
+
   // return a JSON string so that it can be parsed using `fromJson`, e.g. fromJson('["self-hosted", "linux"]')
   return { useRunner: JSON.stringify(useRunner.split(',')), primaryIsOnline };
 }
@@ -42,6 +89,7 @@ async function main() {
       token: core.getInput('github-token', { required: true }),
       primaryRunnerLabels: core.getInput('primary-runner', { required: true }).split(','),
       fallbackRunner: core.getInput('fallback-runner', { required: true }),
+      runnerGroup: core.getInput('runner-group', { required: false }),
     };
 
     const { useRunner, primaryIsOnline, error } = await checkRunner(inputs);
